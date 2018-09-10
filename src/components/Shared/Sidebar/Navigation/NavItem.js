a40b6386f55650a2421d48052a68d537572ff668
Jess Poemape <jesspoemape@gmail.com>
2018-09-09 21:01:23 -0600

    styling problem with employees page

--- src/components/Shared/Sidebar/Navigation/NavItem.js
+++ src/components/Shared/Sidebar/Navigation/NavItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavItem = ({name, svg}) => {
     return (
-        <div className="nav-item">
+        <div className={`nav-item ${window.location.pathname.includes(name) ? 'nav-item-active' : ''}`}>
             <img className="nav-item--svg" src={svg} alt={name}/>
             <div className="nav-item--name">{name}</div>
         </div>
